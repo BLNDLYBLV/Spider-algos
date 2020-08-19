32e41444f28a2d6731c591849f6cdce840399598
Sriram <63253234+BLNDLYBLV@users.noreply.github.com>
2020-08-19 22:05:28 +0530

    Corrected a mistake 😅

--- task-1/task-1-a.cpp
+++ task-1/task-1-a.cpp
@@ -21,30 +21,41 @@ int numbertoarray(int arr[],int n,int a){
 
 int main()
 {
-    int n,alloneflag=1,allzeroflag=1;
+    int n,alloneflag=1,allzeroflag=1,firstoneflag=1;
     cin>>n;
     int given[n];
     string givenstr;
     cin>>givenstr;
+    // firstoneflag=int(givenstr[0])-48;
     for(int i=0;i<n;i++){
         given[i]=int(givenstr[i])-48;
+        // cout<<given[i];
         if(given[i]==0){
             alloneflag=0;
         }
-        if(given[i]==1){
+        if(given[i]==1 && i!=0){
             allzeroflag=0;
         }
     }
     if(alloneflag==1){
         cout<<-1;
     }
-    else if(allzeroflag==1){
-        cout<<"0000 0000";
+    else if(allzeroflag==1 && firstoneflag==1){
+        cout<<"-1";
+    }
+    else if(allzeroflag==1 && firstoneflag==0){
+        for(int i=0;i<n;i++){
+            cout<<'0';
+        }
+        cout<<' ';
+        for(int i=0;i<n;i++){
+            cout<<'0';
+        }
     }
     else
     {
         int givenno=arraytonumber(given,n);
-        // cout<<givenno<<'\n';
+        // cout<<given[0]<<given[1]<<given[2]<<given[3]<<givenno<<'\n';
         int arr1[n],arr2[n];
         numbertoarray(arr1,n,givenno-1);
         numbertoarray(arr2,n,givenno+1);
